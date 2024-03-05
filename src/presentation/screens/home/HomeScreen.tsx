87ae5351723c7221c2da6310371bcb1c22634c7d
Jeral Pineda <jeral.cris@gmail.com>
2024-03-04 22:24:40 -0600

    configurando stack navigator

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 
-export const HomeScree = () => {
+export const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScree</Text>
