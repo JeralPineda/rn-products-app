043cd861d5cfeb14c7bcea0e2463c784ede48fbc
Jeral Pineda <jeral.cris@gmail.com>
2024-03-09 19:03:33 -0600

    feat: add Icon component

---
+++ src/presentation/components/ui/Icon.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {StyleProp, StyleSheet, ViewStyle} from "react-native";
+import {Icon as MyIcon, useTheme} from "@ui-kitten/components";
+
+export interface IconProps {
+  name: string;
+  color?: string;
+  white?: boolean;
+  style?: StyleProp<ViewStyle>;
+}
+
+export default function Icon({name, color, white = false, style}: IconProps) {
+  const theme = useTheme();
+
+  const fillColor = white
+    ? theme["color-info-transparent-100"]
+    : !color
+    ? theme["color-basic-500"]
+    : theme[color] ?? theme["color-basic-500"];
+
+  return (
+    <MyIcon
+      //
+      name={name}
+      fill={fillColor}
+      style={[styles.icon, style]}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 28,
+    height: 28,
+  },
+});
