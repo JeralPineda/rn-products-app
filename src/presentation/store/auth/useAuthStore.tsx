f307fed552fa70197cb6b2651528ee86ac000469
Jeral Pineda <jeral.cris@gmail.com>
2024-03-10 01:25:05 -0600

    feat: add useAuthStore and useUiStore

---
+++ src/presentation/store/auth/useAuthStore.tsx
@@ -0,0 +1,33 @@
+import {authLogin} from "../../../actions/auth";
+import {User} from "../../../domain/entities/user";
+import {AuthStatus} from "../../../infrastructure/interfaces/auth.status";
+
+import {create} from "zustand";
+
+export interface Authstate {
+  status: AuthStatus;
+  token?: string;
+  user?: User;
+
+  login: (email: string, password: string) => Promise<boolean>;
+}
+
+export const useAuthStore = create<Authstate>()((set, get) => ({
+  status: "checking",
+  token: undefined,
+  user: undefined,
+
+  login: async (email: string, password: string) => {
+    const resp = await authLogin(email, password);
+    if (!resp) {
+      set({status: "checking", token: undefined, user: undefined});
+      return false;
+    }
+
+    //TODO Save token and user in storage
+
+    set({status: "authenticated", token: resp.token, user: resp.user});
+
+    return true;
+  },
+}));
