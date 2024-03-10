b8df51fd5a3ff5ab17cdb8d188c60743962fd58a
Jeral Pineda <jeral.cris@gmail.com>
2024-03-10 13:32:14 -0600

    feat: add useUistore

--- src/presentation/store/ui/useUiStore.tsx
+++ src/presentation/store/ui/useUiStore.tsx
@@ -1,1 +1,44 @@
-export const useUistore = {};
+import {create} from "zustand";
+
+export type Notification = {
+  id?: string | number;
+  type: "error" | "success" | "warning" | "info";
+  message: string[];
+};
+
+interface UiState {
+  notifications: Notification[] | null;
+
+  addNotification: (notification: Notification) => void;
+  removeNotification: (id: Notification["id"]) => void;
+  cleanNotifications: () => void;
+}
+
+export const useUistore = create<UiState>()(set => ({
+  notifications: null,
+
+  addNotification: (notification: Notification) => {
+    const newNotification: Notification = {
+      ...notification,
+      id: new Date().getTime(),
+    };
+    set(state => ({
+      notifications:
+        state.notifications !== null
+          ? [...state.notifications, newNotification].slice(-3)
+          : [newNotification],
+    }));
+  },
+
+  removeNotification: (id: Notification["id"]) => {
+    set(state => ({
+      notifications:
+        state.notifications &&
+        state.notifications?.filter(notification => notification.id !== id),
+    }));
+  },
+
+  cleanNotifications: () => {
+    set({notifications: null});
+  },
+}));
