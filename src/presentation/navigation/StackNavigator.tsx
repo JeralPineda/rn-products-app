8f53b6dc3266dbd5af5858be821ba3badc90531d
Jeral Pineda <jeral.cris@gmail.com>
2024-03-09 23:02:50 -0600

    fix: modificando el stack navigator

--- src/presentation/navigation/StackNavigator.tsx
+++ src/presentation/navigation/StackNavigator.tsx
@@ -1,10 +1,15 @@
+// /* eslint-disable react/no-unstable-nested-components */
 import React from "react";
-import {createStackNavigator} from "@react-navigation/stack";
+import {
+  StackCardStyleInterpolator,
+  createStackNavigator,
+} from "@react-navigation/stack";
 import {LoadingScreen} from "../screens/loading/LoadingScreen";
 import {LoginScreen} from "../screens/auth/LoginScreen";
 import {RegisterScreen} from "../screens/auth/RegisterScreen";
 import {HomeScreen} from "../screens/home/HomeScreen";
 import {ProductScreen} from "../screens/product/ProductScreen";
+import {useTheme} from "@ui-kitten/components";
 
 export type RootStackParams = {
   LoadingScreen: undefined;
@@ -16,17 +21,53 @@ export type RootStackParams = {
 
 const Stack = createStackNavigator<RootStackParams>();
 
+const fadeAnimation: StackCardStyleInterpolator = ({current}) => {
+  return {
+    cardStyle: {
+      opacity: current.progress,
+    },
+  };
+};
+
 export const StackNavigator = () => {
+  const theme = useTheme();
+
   return (
     <Stack.Navigator
       initialRouteName="LoginScreen"
       screenOptions={{
         headerShown: false,
+        headerTintColor: theme["color-primary-500"],
+        // cardStyleInterpolator: fadeAnimation,
       }}>
-      <Stack.Screen name="LoginScreen" component={LoginScreen} />
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
-      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      <Stack.Screen
+        options={{
+          title: "",
+          cardStyleInterpolator: fadeAnimation,
+        }}
+        name="LoginScreen"
+        component={LoginScreen}
+      />
+      <Stack.Screen
+        options={{cardStyleInterpolator: fadeAnimation}}
+        name="HomeScreen"
+        component={HomeScreen}
+      />
+      <Stack.Screen
+        options={{cardStyleInterpolator: fadeAnimation}}
+        name="LoadingScreen"
+        component={LoadingScreen}
+      />
+      <Stack.Screen
+        options={{
+          headerShown: true,
+          title: "",
+          headerBackTitle: " ",
+          cardStyleInterpolator: fadeAnimation,
+        }}
+        name="RegisterScreen"
+        component={RegisterScreen}
+      />
       <Stack.Screen name="ProductScreen" component={ProductScreen} />
     </Stack.Navigator>
   );
