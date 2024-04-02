deda07353cc894b91e0b57a361171e8aac8a8c0c
Jeral Pineda <jeral.cris@gmail.com>
2024-04-02 00:07:12 -0600

    feat: Creando interfaces y peticion HTTP de Product

---
+++ src/actions/products/get-products-by-page.ts
@@ -0,0 +1,24 @@
+import {tesloApi} from "../../config/api/tesloApi";
+import {TesloProduct} from "../../infrastructure/interfaces/teslo-products.response";
+import {ProductMapper} from "../../infrastructure/mappers/product.mapper";
+
+export const getProductsByPage = async (page: number, limit: number = 20) => {
+  try {
+    const {data} = await tesloApi.get<TesloProduct[]>(
+      `/products?offset=${page * 10}&limit=${limit}`,
+    );
+
+    const product = data.map(ProductMapper.tesloProductToEntity);
+    console.log(
+      "🚀 get-products-by-page.ts -> #12 -> product ~",
+      JSON.stringify(product, null, 2),
+    );
+    return product;
+  } catch (error) {
+    console.log(
+      "🚀 get-products-by-page.ts -> #5 -> error ~",
+      JSON.stringify(error, null, 2),
+    );
+    throw new Error("Error getting products");
+  }
+};
