1347e8e4f1e84d57539deb518c596d355ccc0191
Jeral Pineda <jeral.cris@gmail.com>
2024-04-03 22:29:29 -0600

    feat: add AppBar MainLayout

---
+++ src/presentation/layouts/MainLayout.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable curly */
+/* eslint-disable react/no-unstable-nested-components */
+import React from "react";
+import {StyleSheet} from "react-native";
+import {
+  Divider,
+  Layout,
+  TopNavigation,
+  TopNavigationAction,
+} from "@ui-kitten/components";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+import {useNavigation} from "@react-navigation/native";
+import Icon from "../components/ui/Icon";
+
+interface MainLayoutProps {
+  title: string;
+  subTitle?: string;
+  rightAction?: () => void;
+  rightActionIcon?: string;
+
+  children: React.ReactNode;
+}
+
+export const MainLayout = ({
+  title,
+  subTitle,
+  rightAction,
+  rightActionIcon,
+  children,
+}: MainLayoutProps) => {
+  const {top} = useSafeAreaInsets();
+  const {canGoBack, goBack} = useNavigation();
+
+  const renderBackActions = () => (
+    <TopNavigationAction
+      icon={<Icon name="arrow-back-outline" />}
+      onPress={goBack}
+    />
+  );
+
+  const RenderRightAction = () => {
+    if (rightAction === undefined || rightActionIcon === undefined) return null;
+
+    return (
+      <TopNavigationAction
+        onPress={rightAction}
+        icon={<Icon name={rightActionIcon} />}
+      />
+    );
+  };
+
+  return (
+    <Layout style={{...styles.container, paddingTop: top}}>
+      <TopNavigation
+        title={title}
+        subtitle={subTitle}
+        alignment="center"
+        accessoryLeft={canGoBack() ? renderBackActions : undefined}
+        accessoryRight={() => <RenderRightAction />}
+      />
+      <Divider />
+
+      <Layout style={styles.container}>{children}</Layout>
+    </Layout>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+  },
+});
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import {Button, Icon, Layout, Text} from "@ui-kitten/components";
-import {useAuthStore} from "../../store";
+import {Text} from "@ui-kitten/components";
 import {getProductsByPage} from "../../../actions/products/get-products-by-page";
 import {useQuery} from "@tanstack/react-query";
+import {MainLayout} from "../../layouts/MainLayout";
 
 export const HomeScreen = () => {
-  const {logout} = useAuthStore();
-
   const {isLoading, data: products = []} = useQuery({
     queryKey: ["products", "infinite"],
     staleTime: 1000 * 60 * 60, // 1hour
@@ -14,14 +12,10 @@ export const HomeScreen = () => {
   });
 
   return (
-    <Layout style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+    <MainLayout
+      title="TesloShop - Productos"
+      subTitle="Aplicación administrativa">
       <Text>HomeScree</Text>
-
-      <Text>{JSON.stringify(products, null, 2)}</Text>
-
-      {/* <Button onPress={logout} accessoryLeft={<Icon name="lock-outline" />}>
-        Cerrar sesión
-      </Button> */}
-    </Layout>
+    </MainLayout>
   );
 };
