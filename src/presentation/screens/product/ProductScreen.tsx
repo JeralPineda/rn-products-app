6f52c6123fb02d9a920948889ec6322860087cc7
Jeral Pineda <jeral.cris@gmail.com>
2024-04-06 19:37:36 -0600

    style: diseño de la pagina del producto

--- src/presentation/screens/product/ProductScreen.tsx
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -1,10 +1,13 @@
-import {Text} from "@ui-kitten/components";
 import React, {useRef} from "react";
-import {MainLayout} from "../../layouts/MainLayout";
+import {FlatList, ScrollView, StyleSheet} from "react-native";
 import {useQuery} from "@tanstack/react-query";
-import {getProductById} from "../../../actions/products/get-product-by-id";
 import {StackScreenProps} from "@react-navigation/stack";
+
+import {MainLayout} from "../../layouts/MainLayout";
+import {getProductById} from "../../../actions/products/get-product-by-id";
 import {RootStackParams} from "../../navigation/StackNavigator";
+import {Input, Layout} from "@ui-kitten/components";
+import {FadeInImage} from "../../components/ui/FadeInImage";
 
 interface ProductScreenProps
   extends StackScreenProps<RootStackParams, "ProductScreen"> {}
@@ -26,7 +29,72 @@ export const ProductScreen = ({route}: ProductScreenProps) => {
 
   return (
     <MainLayout title={product.title} subTitle={`Precio: ${product.price}`}>
-      <Text>ProductScreen</Text>
+      <ScrollView style={styles.container}>
+        {/* Imágenes del producto */}
+        <Layout>
+          {/* Validar cuando no hay imágenes */}
+          <FlatList
+            data={product.images}
+            horizontal
+            keyExtractor={item => item}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({item}) => (
+              <FadeInImage
+                uri={item}
+                style={{width: 300, height: 300, marginHorizontal: 7}}
+              />
+            )}
+          />
+        </Layout>
+
+        {/* Formulario */}
+        <Layout style={{marginHorizontal: 10}}>
+          <Input
+            label="Titulo"
+            value={product.title}
+            style={{marginVertical: 5}}
+          />
+          <Input
+            label="Slug"
+            value={product.slug}
+            style={{marginVertical: 5}}
+          />
+          <Input
+            label="Descripcion"
+            value={product.description}
+            multiline
+            numberOfLines={5}
+            style={{marginVertical: 5}}
+          />
+        </Layout>
+
+        {/*  Detalle */}
+        <Layout style={styles.detail}>
+          <Input
+            label="Precio"
+            value={product.price.toString()}
+            style={{flex: 1}}
+          />
+          <Input
+            label="Inventario"
+            value={product.stock.toString()}
+            style={{flex: 1}}
+          />
+        </Layout>
+        <Layout style={{height: 200}} />
+      </ScrollView>
     </MainLayout>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  detail: {
+    marginVertical: 5,
+    marginHorizontal: 15,
+    flexDirection: "row",
+    gap: 10,
+  },
+});
